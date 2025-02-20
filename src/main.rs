312bada1873156bb73e83d90e92a27e8dad0f658
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-02-20 17:20:15 +0900

    delete

--- src/main.rs
+++ src/main.rs
@@ -12,26 +12,41 @@ fn main() {
         let action = input[0].clone();
 
         if action == "create" {
-            let task = input[1].clone();
-            create_todo(&mut todos, task);
+            let todo = input[1].clone();
+            create_todo(&mut todos, todo);
         } else if action == "read" {
             read_todo(&todos);
         } else if action == "quit" {
             break;
+        } else if action == "delete" {
+            let todo_to_delete = input[1].clone();
+            delete_todo(&mut todos, todo_to_delete);
         } else {
             eprintln!("please type proper action");
         }
     }
 }
 
-fn create_todo(todos: &mut Vec<String>, task: String) {
-    todos.push(task);
+fn create_todo(todos: &mut Vec<String>, todo: String) {
+    todos.push(todo);
 }
 
 fn read_todo(todos: &Vec<String>) {
     print!("ToDos: ");
     for todo in todos.iter() {
-        print!("{}, ", todo);
+        print!("{} ", todo);
     }
     print!("\n");
+}
+
+fn delete_todo(todos: &mut Vec<String>, todo_to_delete: String) {
+    let mut delete_index: Vec<usize> = vec![];
+    for (index, todo) in todos.iter_mut().enumerate() {
+        if *todo == todo_to_delete {
+            delete_index.push(index);
+        }
+    }
+    for index in delete_index.iter() {
+        todos.remove(*index);
+    }
 }
